5993c3d87fe2d89216530198f1e3156ad23afcd3
faker <2321584752@qq.com>
2019-05-16 20:57:07 +0800

    初始化完成

---
+++ src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>首页</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  methods: {},
+
+  created() {
+    // let app = getApp()
+  }
+};
+</script>
+
+<style scoped>
+</style>
